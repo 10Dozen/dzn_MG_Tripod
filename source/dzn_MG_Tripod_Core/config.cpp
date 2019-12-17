6a91e80087fbd08d8379408eaca2acd1da467222
10Dozen <steel.mjr@gmail.com>
2019-12-17 19:24:30 +0300

    Hold gestures
    - Hold gestures for Stand and Prone

--- source/dzn_MG_Tripod_Core/config.cpp
+++ source/dzn_MG_Tripod_Core/config.cpp
@@ -5,13 +5,19 @@ class CfgPatches
 		units[] = {};
 		weapons[] = {
 			"dzn_MG_Tripod_Universal"
-			,"dzn_MG_Tripod_m240mount"
-			,"dzn_MG_Tripod_m60mount"
+			,"dzn_MG_Tripod_M122A1_M240mount"
+			,"dzn_MG_Tripod_M122A1_M60mount"
+			
 			,"dzn_MG_Tripod_Universal_Carry"
+			,"dzn_MG_Tripod_M122A1_M240mount_Carry"
+			,"dzn_MG_Tripod_M122A1_M60mount_Carry"
 		};
 		requiredVersion = 0.1;
 		requiredAddons[] = {
-			"CBA_MAIN"
+			"A3_Weapons_F"
+			,"A3_Data_F"
+			,"A3_Anims_F"
+			,"CBA_MAIN"
 			,"CBA_jr"
 			,"dzn_MG_Tripod"
 			,"dzn_MG_Tripod_A3"
@@ -22,22 +28,20 @@ class CfgPatches
 	};
 };
 
-/*
 class Extended_PostInit_EventHandlers
 {
 	class dzn_MG_Tripod
 	{
-		init = call compile preprocessFileLineNumbers "\dzn_MG_Tripod_Core\Init.sqf";
+		// init = call compile preprocessFileLineNumbers "\dzn_MG_Tripod_Core\Init.sqf";
 	};
 };
 class Extended_PreInit_EventHandlers
 {
 	class dzn_MG_Tripod
 	{
-		init = call compile preprocessFileLineNumbers "\dzn_MG_Tripod_Core\Settings.sqf";
+		// init = call compile preprocessFileLineNumbers "\dzn_MG_Tripod_Core\Settings.sqf";
 	};
 };
-*/
 
 class CfgWeapons
 {
@@ -123,3 +127,198 @@ class CfgWeapons
 		};
 	};
 };
+
+class CfgMovesBasic
+{
+	class DefaultDie;
+	class ManActions
+	{
+		dzn_MG_Tripod_GestureHoldType1			= "dzn_MG_Tripod_GestureHoldType1";
+		dzn_MG_Tripod_GestureHoldType2			= "dzn_MG_Tripod_GestureHoldType2";
+		dzn_MG_Tripod_GestureHoldType3			= "dzn_MG_Tripod_GestureHoldType3";
+		dzn_MG_Tripod_GestureHoldType4			= "dzn_MG_Tripod_GestureHoldType4";
+		dzn_MG_Tripod_GestureHoldProneType1		= "dzn_MG_Tripod_GestureHoldProneType1";
+		dzn_MG_Tripod_GestureHoldProneType2		= "dzn_MG_Tripod_GestureHoldProneType2";
+	};
+	class Actions
+	{
+		class NoActions: ManActions
+		{
+			dzn_MG_Tripod_GestureHoldType1[]			= {"dzn_MG_Tripod_GestureHoldType1","Gesture"};
+			dzn_MG_Tripod_GestureHoldType2[]			= {"dzn_MG_Tripod_GestureHoldType2","Gesture"};
+			dzn_MG_Tripod_GestureHoldType3[]			= {"dzn_MG_Tripod_GestureHoldType3","Gesture"};
+			dzn_MG_Tripod_GestureHoldType4[]			= {"dzn_MG_Tripod_GestureHoldType4","Gesture"};
+			dzn_MG_Tripod_GestureHoldProneType1[]		= {"","Gesture"};
+			dzn_MG_Tripod_GestureHoldProneType2[]		= {"","Gesture"};
+		};
+		class RifleBaseStandActions;
+		class RifleProneActions: RifleBaseStandActions // _Prone
+		{
+			dzn_MG_Tripod_GestureHoldType1[]			= {"dzn_MG_Tripod_GestureHoldProneType1","Gesture"};
+			dzn_MG_Tripod_GestureHoldType2[]			= {"dzn_MG_Tripod_GestureHoldProneType2","Gesture"};
+			dzn_MG_Tripod_GestureHoldType3[]			= {"dzn_MG_Tripod_GestureHoldProneType2","Gesture"};
+			dzn_MG_Tripod_GestureHoldType4[]			= {"dzn_MG_Tripod_GestureHoldProneType1","Gesture"};
+			dzn_MG_Tripod_GestureHoldProneType1[]		= {"dzn_MG_Tripod_GestureHoldProneType1","Gesture"};
+			dzn_MG_Tripod_GestureHoldProneType2[]		= {"dzn_MG_Tripod_GestureHoldProneType2","Gesture"};
+		};
+		class RifleAdjustProneBaseActions;
+		class RifleAdjustRProneActions: RifleAdjustProneBaseActions // _Context
+		{
+			dzn_MG_Tripod_GestureHoldType1[]			= {"dzn_MG_Tripod_GestureHoldType1_Context","Gesture"};
+			dzn_MG_Tripod_GestureHoldType2[]			= {"dzn_MG_Tripod_GestureHoldType2_Context","Gesture"};
+			dzn_MG_Tripod_GestureHoldType3[]			= {"dzn_MG_Tripod_GestureHoldType3_Context","Gesture"};
+			dzn_MG_Tripod_GestureHoldType4[]			= {"dzn_MG_Tripod_GestureHoldType4_Context","Gesture"};
+			dzn_MG_Tripod_GestureHoldProneType1[]		= {"","Gesture"};
+			dzn_MG_Tripod_GestureHoldProneType2[]		= {"","Gesture"};
+		};
+		class RifleAdjustLProneActions: RifleAdjustProneBaseActions // _Context
+		{
+			dzn_MG_Tripod_GestureHoldType1[]			= {"dzn_MG_Tripod_GestureHoldType1_Context","Gesture"};
+			dzn_MG_Tripod_GestureHoldType2[]			= {"dzn_MG_Tripod_GestureHoldType2_Context","Gesture"};
+			dzn_MG_Tripod_GestureHoldType3[]			= {"dzn_MG_Tripod_GestureHoldType3_Context","Gesture"};
+			dzn_MG_Tripod_GestureHoldType4[]			= {"dzn_MG_Tripod_GestureHoldType4_Context","Gesture"};
+			dzn_MG_Tripod_GestureHoldProneType1[]		= {"","Gesture"};
+			dzn_MG_Tripod_GestureHoldProneType2[]		= {"","Gesture"};
+
+			class RifleAdjustFProneActions: RifleAdjustProneBaseActions
+			{
+				dzn_MG_Tripod_GestureHoldType1[]			= {"dzn_MG_Tripod_GestureHoldType1_Context","Gesture"};
+				dzn_MG_Tripod_GestureHoldType2[]			= {"dzn_MG_Tripod_GestureHoldType2_Context","Gesture"};
+				dzn_MG_Tripod_GestureHoldType3[]			= {"dzn_MG_Tripod_GestureHoldType3_Context","Gesture"};
+				dzn_MG_Tripod_GestureHoldType4[]			= {"dzn_MG_Tripod_GestureHoldType4_Context","Gesture"};
+				dzn_MG_Tripod_GestureHoldProneType1[]		= {"","Gesture"};
+				dzn_MG_Tripod_GestureHoldProneType2[]		= {"","Gesture"};
+			};
+		};
+	};
+};
+
+#define GESTURE_MACRO(X)	\
+	class dzn_MG_Tripod_Gesture##X: dzn_MG_Tripod_GestureHoldType1 { file = dzn_MG_Tripod_Core\anim\Hold\##X##.rtm; }; \
+	class dzn_MG_Tripod_Gesture##X##_Context: dzn_MG_Tripod_Gesture##X { mask = "handsWeapon_context"; }
+
+class CfgGesturesMale
+{
+	class Default;
+	class States
+	{
+		/*
+		 *	HoldType1 	-- default for CUP GMPG (M60, M249, etc) and A3 MGs
+		 *	HoldType2	-- CUP M249 solid stock (a bit lower and rotated clockwise)
+		 *	HoldType3	-- CUP M249 Para / PKM/PKP (a bit lower and not rotated)
+		 *	HoldType4	-- a bit higher than default (suitable for IAR type mgs and assault rifles - RPK, MG36, etc).
+		 */
+		class dzn_MG_Tripod_GestureHoldType1: Default
+		{
+			file = "dzn_MG_Tripod_Core\anim\Hold\HoldType1.rtm";
+			looped = 1;
+			speed = 1;
+			mask = "leftHand";
+			leftHandIKBeg = 1;
+			leftHandIKEnd = 1;
+			leftHandIKCurve[] = {0.0};
+			enableOptics = 1;
+			canPullTrigger = 1;
+			disableWeapons = 0;
+		};
+		class dzn_MG_Tripod_GestureHoldType1_Context: dzn_MG_Tripod_GestureHoldType1
+		{
+			mask = "handsWeapon_context";
+		};
+		
+		GESTURE_MACRO(HoldType2);
+		GESTURE_MACRO(HoldType3);
+		GESTURE_MACRO(HoldType4);		
+		
+		/*
+		 *	HoldProneType1 	-- default prone (for all mgs, both CUP and A3)
+		 *	HoldProneType2 	-- CUP M249 (or other MGs with stock close to shoulder)
+		 */
+		class dzn_MG_Tripod_GestureHoldProneType1: Default
+		{
+			file = "dzn_MG_Tripod_Core\anim\Hold\HoldProneType1.rtm";
+			looped = 1;
+			speed = 1;
+			mask = "leftHand"; 
+			leftHandIKBeg = 1;
+			leftHandIKCurve[] = {0};
+			leftHandIKEnd = 1;
+			enableOptics = 1;
+			canPullTrigger = 1;
+			disableWeapons = 0;
+		};
+		class dzn_MG_Tripod_GestureHoldProneType2: dzn_MG_Tripod_GestureHoldProneType1
+		{
+			file = "dzn_MG_Tripod_Core\anim\Hold\HoldProneType2.rtm";
+		};
+	};
+};
+
+/*****
+	Weapon					| Anim(Good)									| Prone
+	----------------------------------------------------------------------------------------------------
+	L110/M249Para			| HoldTop3										| _v16
+	M240					| HoldTop1 										| 
+	L86						| -- need special anim --						| 
+	M249					| HoldTop2										| _v16
+	M249 PIP Stock			| HoldTop2										| _v16
+	M60						| HoldTop1										| _v17
+	M60E4					| HoldTop1										| _v17
+	MG3						| HoldTop1										| _v17
+	MG36					| HoldTop4										| 
+	Mk48					| HoldTop2										| _v16
+	PKP						| HoldTop3  									| _v16 _v17
+	RPK74					| HoldTop4										| _v17
+	XM8 SAW					| HoldTop4										| 
+	
+	### Vanilla 
+	RPK12					| HoldTop1										| _v17
+	Zafir					| HoldTop1										| 
+	Mk200					| HoldTop1										| 
+
+	Top1 - Type1
+	Top2 - Type2
+	Top3 - Type3
+	Top4 - Type4
+	
+	_v17 -- ProneType1
+	_v16 -- ProneType2
+	
+	Mk12 | HoldTop4
+	SR25 | HoldTop1
+	
+	ProneAnim:
+		_v12 	- is +-OK
+		_v15	- is +-OK (better)
+		
+		
+		_v16 	- Per
+	
+	dzn_classes = [
+		"CUP_lmg_L110A1"
+		, "CUP_lmg_L7A2"
+		, "CUP_arifle_L86A2"
+		, "CUP_lmg_m249_pip1"
+		, "CUP_lmg_m249_pip3"		
+		, "CUP_lmg_M60"
+		, "CUP_lmg_M60E4_norail"		
+		, "CUP_lmg_MG3_rail"		
+		, "CUP_arifle_MG36_camo"		
+		, "CUP_lmg_Mk48_nohg_od"		
+		, "CUP_lmg_Pecheneg"
+		, "CUP_arifle_RPK74M"
+		, "CUP_arifle_xm8_SAW"
+		
+		
+		, "arifle_RPK12_lush_F"
+		, "LMG_Zafir_F"
+		, "LMG_Mk200_F"
+	];
+	
+	{
+		player addAction [" ## " + _x, { params ["", "", "", "_w"]; [player, _w, 3] call BIS_fnc_addWeapon; }, _x];
+	} forEach dzn_classes;
+	
+	player playAction "dzn_MG_Tripod_GestureHoldM240";
+
+*/
